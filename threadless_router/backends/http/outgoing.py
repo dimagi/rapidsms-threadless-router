df3735b68796e5af84e1eb4cd26978fa6146b13a
Giovanni Capalbo <gcapalbo@dimagi.com>
2011-10-24 15:07:15 -0400

    Allows outgoing identity and text parameter names to be specified in settings for http backend

--- threadless_router/backends/http/outgoing.py
+++ threadless_router/backends/http/outgoing.py
@@ -5,15 +5,18 @@ from threadless_router.backends.base import BackendBase
 
 
 EXAMPLE_URL = 'http://127.0.0.1/'
-
+DEFAULT_OUTGOING_IDENTITY_PARAM = "identity"
+DEFAULT_OUTGOING_TEXT_PARAM = "text"
 
 class HttpBackend(BackendBase):
 
     def send(self, message):
         self.info('Sending message: %s' % message)
-        url = self._config.get('outgoing_url', EXAMPLE_URL)
-        data = {'text': message.text,
-                'identity': message.connection.identity}
+        outgoing_identity = self.b_config.get("outgoing_identity", DEFAULT_OUTGOING_IDENTITY_PARAM)
+        outgoing_text = self.b_config.get("outgoing_text", DEFAULT_OUTGOING_TEXT_PARAM)
+        url = self.b_config.get('outgoing_url', EXAMPLE_URL)
+        data = {outgoing_text: message.text,
+                outgoing_identity: message.connection.identity}
         try:
             self.debug('Opening URL: %s' % url)
             response = urllib2.urlopen(url, urllib.urlencode(data))
